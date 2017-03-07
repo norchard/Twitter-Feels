d0f63fc8b13d427e6eeb5ce865340e8230234729
Nicole Orchard <norchard24@gmail.com>
2017-03-07 10:27:22 -0500

    Cleaned up commented code

--- lang_processing.rb
+++ lang_processing.rb
@@ -1,39 +1,18 @@
 #!/usr/bin/env ruby
 
-# Imports the Google Cloud client library
 require "google/cloud/language"
 require "json"
 require "dotenv"
 
-# f = open("tweets", "r+")
-# json = f.read
-# f.close
-
 json = $stdin.read
 
 tweets = JSON.parse(json, :symbolize_names => true)
-
-# puts tweets[0][:text]
 
 # Your Google Cloud Platform project ID
 project_id = ENV['GOOGLE_PROJECT_ID']
 
 # Instantiates a client
 language = Google::Cloud::Language.new project: project_id
-
-# # The text to analyze
-# text     = "I'm hungry. Let's eat breakfast."
-# document = language.document text
-#
-# # Detects the sentiment of the text
-# sentiment = document.sentiment
-#
-# puts "Text: #{text}"
-# puts "Score: #{sentiment.score}, #{sentiment.magnitude}"
-
-# count = 0
-# score = 0
-# magnitude = 0
 
 tweets.each do |tweet|
   $stderr.print "."
@@ -47,20 +26,6 @@ tweets.each do |tweet|
 
   syntax = document.syntax
   syntax.tokens.each {|token| tweet[:nouns] << token.text.downcase if token.part_of_speech.tag.to_s == "NOUN"}
-
-  # count += 1
-  # score += sentiment.score
-  # magnitude += sentiment.magnitude
-
-  # puts "Text: #{text}"
-  # puts "Score: #{sentiment.score}, #{sentiment.magnitude}"
 end
 
-# f = File.new("data/tweet_feels.json", "w+")
-# f.write(JSON.pretty_generate(tweets));
-# f.close
-
 puts JSON.pretty_generate(tweets);
-
-# puts "Average score: #{score / count}"
-# puts "Average magnitude: #{magnitude / count}"
